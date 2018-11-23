b5ecdf943c603d710ce991003a6bd7165281a532
Lester Lee <ll6@williams.edu>
2018-11-23 01:14:55 -0500

    Add camera fade

--- js/interaction.js
+++ js/interaction.js
@@ -5,11 +5,7 @@ const gameInteractions = {
   "Door--start": function (scene) {
     speak(scene, [
       "The door looms before you.",
-      "The paint looks aged.",
-      "Yeah Lorem ipsum",
-      "you know it baby",
-      "ehre's a lot of text ooo boy",
-      "whatup in the club",
+      "You're not sure how long this building has been here.",
       "Will you try going inside?",
       "A: Yes B: No"
     ],
@@ -18,6 +14,7 @@ const gameInteractions = {
         "interact": function () {
           ageSpeech();
           renderSpeech();
+          scene.cameras.main.fade(500);
           scene.scene.start("scene_projector");
         },
         "cancel": function(){
